b35590330327bf73d9aaeba9f764adf2ece1b6e9
Gabatawr <gabatawr@gmail.com>
2021-03-16 18:29:51 +0200

    home save

---
+++ Infrastructure/Commands/GoCommand.cs
@@ -0,0 +1,21 @@
+﻿using Chatyx.Infrastructure.Commands.Base;
+using Chatyx.ViewModels;
+
+namespace Chatyx.Infrastructure.Commands
+{
+    class GoCommand : Command
+    {
+        private readonly MainWindowViewModel vm;
+        public GoCommand(MainWindowViewModel vm) => this.vm = vm;
+
+        public override void Execute(object e)
+        {
+
+        }
+
+        public override bool CanExecute(object e)
+        {
+            return true;
+        }
+    }
+}
